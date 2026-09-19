7202d0cca3737a0568ffaceb280a917ddd92b06f
zzbtefu <149356280+zzbtefu@users.noreply.github.com>
2026-09-19 20:30:58 +0900

    とりあえず動けるようになった．

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private float speed = 3.0f;
+
+    private float inputx;
+    private float inputz;
+
+    private Vector3 velocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        velocity = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        inputx = Input.GetAxis("Horizontal");
+        inputz = Input.GetAxis("Vertical");
+        velocity = new Vector3(inputx, 0, inputz) * speed;
+        this.transform.Translate(velocity * Time.deltaTime);
     }
 }
